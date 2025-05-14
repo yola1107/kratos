e4f3c17f639dd1d59c91fe4bc138e84dac4ea8fd
znyh <1518684794@qq.com>
2025-05-15 01:56:52 +0800

    prod without color level

--- library/log/zap/zap.go
+++ library/log/zap/zap.go
@@ -5,7 +5,6 @@ import (
 	"io"
 	"os"
 	"path/filepath"
-	"runtime"
 	"strings"
 	"sync"
 	"time"
@@ -191,10 +190,6 @@ func NewLogger(opts ...Option) (*Logger, error) {
 		fileEncoderCfg := encoderCfg
 		fileEncoderCfg.EncodeCaller = customCallerEncoder
 		fileEncoderCfg.EncodeLevel = customLevelEncoder
-		if runtime.GOOS != "windows" {
-			fileEncoderCfg.EncodeCaller = customColorCallerEncoder
-			fileEncoderCfg.EncodeLevel = customColorLevelEncoder
-		}
 		fileEnc := zapcore.NewConsoleEncoder(fileEncoderCfg)
 
 		if cfg.directory != "" && cfg.filename != "" {
