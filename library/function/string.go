03d0ec20ee6f653d80d845c0b511cb5e74162c49
zhangyh <1518684794@qq.com>
2025-05-16 18:43:04 +0800

    api-server base function

---
+++ library/function/string.go
@@ -0,0 +1,78 @@
+package function
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+)
+
+//ToString .
+func ToString(v any) string {
+	return fmt.Sprintf("%v", v)
+}
+
+func IntToStr(src int) string {
+	return strconv.Itoa(src)
+}
+
+func Int32ToStr(src int32) string {
+	return strconv.Itoa(int(src))
+}
+
+func Int64ToStr(src int64) string {
+	return strconv.FormatInt(src, 10)
+}
+
+func Float64ToStr(f float64) string {
+	return strconv.FormatFloat(f, 'f', 2, 64)
+}
+
+func Float64ToStrN(f float64, prec int) string {
+	return strconv.FormatFloat(f, 'f', prec, 64)
+}
+
+func StrToInt(src string) int {
+	if src == "" {
+		return 0
+	}
+	dst, err := strconv.Atoi(src)
+	if err != nil {
+		log.Printf("str to int error(%v).", err)
+		return 0
+	}
+	return dst
+}
+
+func StrToInt32(src string) int32 {
+	if src == "" {
+		return 0
+	}
+	dst, err := strconv.Atoi(src)
+	if err != nil {
+		log.Printf("str to int32 error(%v).", err)
+		return 0
+	}
+	return int32(dst)
+}
+
+func StrToInt64(src string) int64 {
+	if src == "" {
+		return 0
+	}
+	dst, err := strconv.ParseInt(src, 10, 64)
+	if err != nil {
+		log.Printf("str to int64 error(%v).", err)
+		return 0
+	}
+	return dst
+}
+
+func StrToFloat64(src string) float64 {
+	if src == "" {
+		return 0
+	}
+	if v, err := strconv.ParseFloat(src, 64); err == nil {
+		return v
+	}
+	return 0
+}
--- library/function/time.go
+++ library/function/time.go
@@ -4,25 +4,43 @@ import (
 	"time"
 )
 
-// 获取当前秒
+//GetTick 毫秒级时间戳
+func GetTick() int64 {
+	return time.Now().UnixNano() / 1e6
+}
+
+//GetCurSec 获取当前时间 s
 func GetCurSec() int64 {
 	return time.Now().Unix()
 }
 
-// 获取当前纳秒
+//GetCurNanoSec 获取当前纳秒
 func GetCurNanoSec() int64 {
 	return time.Now().UnixNano()
 }
 
-// 获取当前毫秒
-func GetCurTicks() int64 {
-	return time.Now().UnixNano() / 1e6
+//GetCurDate 返回当前的日期（零时零分）
+func GetCurDate() time.Time {
+	now := time.Now()
+	return time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
 }
 
 // 获取时间日期
-func GetDate() time.Time {
-	then := time.Date(
-		2017, 06, 21, 20, 34, 58, 0, time.UTC)
+//func GetDate() time.Time {
+//	then := time.Date(
+//		2017, 06, 21, 20, 34, 58, 0, time.UTC)
+//
+//	return then
+//}
 
-	return then
+func FormatDate(t time.Time) string {
+	return t.Format("2006-01-02")
+}
+
+func FormatDateTime(t time.Time) string {
+	return t.Format("2006-01-02 15:04:05")
+}
+
+func FormatTime(t time.Time, layout string) string {
+	return t.Format(layout)
 }
