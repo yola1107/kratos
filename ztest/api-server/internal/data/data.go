5b81a2a261fb0b04bce8dcb8539e441bbc367261
zhangyh <1518684794@qq.com>
2025-06-17 18:33:25 +0800

    Room Restruct-5

--- ztest/api-server/internal/data/data.go
+++ ztest/api-server/internal/data/data.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"github.com/google/wire"
+	"github.com/yola1107/kratos/v2/ztest/api-server/internal/biz"
 
 	"github.com/yola1107/kratos/v2/log"
 	"github.com/yola1107/kratos/v2/ztest/api-server/internal/conf"
@@ -9,6 +10,19 @@ import (
 
 // ProviderSet is data providers.
 var ProviderSet = wire.NewSet(NewData, NewDataRepo)
+
+type dataRepo struct {
+	data *Data
+	log  *log.Helper
+}
+
+// NewDataRepo .
+func NewDataRepo(data *Data, logger log.Logger) biz.DataRepo {
+	return &dataRepo{
+		data: data,
+		log:  log.NewHelper(logger),
+	}
+}
 
 // Data .
 type Data struct {
