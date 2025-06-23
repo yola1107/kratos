c74c96227b4020f00de4a36e68bfa44cac239c5c
zhangyh <1518684794@qq.com>
2025-06-23 14:35:02 +0800

    ext Rand 线程不安全

--- library/ext/rand.go
+++ library/ext/rand.go
@@ -2,14 +2,29 @@ package ext
 
 import (
 	"math/rand"
+	"sync"
 	"time"
 
 	"golang.org/x/exp/constraints"
 )
 
 var (
-	secureRand = rand.New(rand.NewSource(time.Now().UnixNano()))
+	randPool = sync.Pool{
+		New: func() any {
+			// 用纳秒时间戳 + goroutine id/hash作为种子更均匀
+			seed := time.Now().UnixNano() ^ int64(rand.Intn(1<<20))
+			return rand.New(rand.NewSource(seed))
+		},
+	}
 )
+
+func getRand() *rand.Rand {
+	return randPool.Get().(*rand.Rand)
+}
+
+func putRand(r *rand.Rand) {
+	randPool.Put(r)
+}
 
 // IsHit 判断概率为 v% 的事件是否命中 (v 范围 [0,100])
 func IsHit(v int) bool {
@@ -19,7 +34,9 @@ func IsHit(v int) bool {
 	if v >= 100 {
 		return true
 	}
-	return secureRand.Intn(100) < v
+	r := getRand()
+	defer putRand(r)
+	return r.Intn(100) < v
 }
 
 // IsHitFloat 判断概率为 v 的事件是否命中 (v 范围 [0,1])
@@ -30,7 +47,9 @@ func IsHitFloat(v float64) bool {
 	if v >= 1 {
 		return true
 	}
-	return secureRand.Float64() < v
+	r := getRand()
+	defer putRand(r)
+	return r.Float64() < v
 }
 
 // RandFloat 生成 [min, max) 范围的随机浮点数
@@ -38,7 +57,9 @@ func RandFloat(min, max float64) float64 {
 	if max <= min {
 		return min
 	}
-	return secureRand.Float64()*(max-min) + min
+	r := getRand()
+	defer putRand(r)
+	return r.Float64()*(max-min) + min
 }
 
 // RandInt 生成 [min, max) 范围的随机整数
@@ -46,7 +67,9 @@ func RandInt[T constraints.Integer](min T, max T) T {
 	if max <= min {
 		return min
 	}
-	num := secureRand.Int63n(int64(max - min))
+	r := getRand()
+	defer putRand(r)
+	num := r.Int63n(int64(max - min))
 	return min + T(num)
 }
 
@@ -55,6 +78,8 @@ func RandIntInclusive[T constraints.Integer](min T, max T) T {
 	if max <= min {
 		return min
 	}
-	num := secureRand.Int63n(int64(max - min + 1))
+	r := getRand()
+	defer putRand(r)
+	num := r.Int63n(int64(max - min + 1))
 	return min + T(num)
 }
