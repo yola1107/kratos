26059d01b6f6a06244849fa462ad532be01809a4
znyh <1518684794@qq.com>
2025-07-13 06:07:35 +0800

    whot proto整理- 13-1 handler.go

--- ztest/game/whot/internal/biz/table/handle.go
+++ ztest/game/whot/internal/biz/table/handle.go
@@ -1,10 +1,8 @@
 package table
 
 import (
-	"github.com/yola1107/kratos/v2/library/ext"
 	"github.com/yola1107/kratos/v2/log"
 	v1 "github.com/yola1107/kratos/v2/ztest/game/whot/api/helloworld/v1"
-	"github.com/yola1107/kratos/v2/ztest/game/whot/internal/biz/calc"
 	"github.com/yola1107/kratos/v2/ztest/game/whot/internal/biz/player"
 	"github.com/yola1107/kratos/v2/ztest/game/whot/pkg/codes"
 )
@@ -50,376 +48,235 @@ func (t *Table) OnOffline(p *player.Player) bool {
 	return true
 }
 
-func (t *Table) OnActionReq(p *player.Player, in *v1.ActionReq, timeout bool) (ok bool) {
-	if p == nil || !p.IsGaming() || len(t.GetGamers()) <= 1 {
+// OnPlayerActionReq 处理客户端请求的操作
+func (t *Table) OnPlayerActionReq(p *player.Player, in *v1.PlayerActionReq, timeout bool) (ok bool) {
+	if p == nil || !p.IsGaming() || len(t.GetGamers()) <= 1 || p.GetChairID() != t.active {
+		return
+	}
+	if s := t.stage.GetState(); s == StWait || s == StReady || s == StWaitEnd || s == StEnd {
 		return
 	}
 
-	stage := t.stage.GetState()
-	if stage == StWait || stage == StReady || stage == StWaitEnd || stage == StEnd {
-		return
-	}
-
-	if timeout && p.IsRobot() {
-		log.Errorf("OnActionReq err. tb:%+v, p:%v, ac=%v, CanOp=%v ",
-			t.Desc(), p.Desc(), in.Action, t.getCanOp(p))
-	}
 	// log.Debugf("=> p:%v, ac=%v CanOp=%v, gamer=%+v timeout=%v",
 	// 	p.Desc(), in.Action, t.getCanOp(p), len(t.GetGamers()), timeout)
 
 	switch in.Action {
-	case v1.ACTION_SEE:
-		t.handleSee(p, timeout)
-	case v1.ACTION_PACK:
-		t.handlePack(p, in, timeout)
-	case v1.ACTION_CALL, v1.ACTION_RAISE:
-		t.handleCall(p, in, timeout)
-	case v1.ACTION_SHOW:
-		t.handleShow(p, in, timeout)
-	case v1.ACTION_SIDE:
-		t.handleSideShow(p, in, timeout)
-	case v1.ACTION_SIDE_REPLY:
-		t.handleSideShowReply(p, in, timeout)
+	case v1.ACTION_PLAY_CARD:
+		if !t.canOutCard(t.currCard, p.GetCards(), in.OutCard) {
+			log.Errorf("playCard err: curr=%v out=%v hand=%v", t.currCard, in.OutCard, p.GetCards())
+			return
+		}
+		t.onPlayCard(p, in.OutCard)
+
+	case v1.ACTION_DRAW_CARD:
+		if !t.canDrawCard(p) {
+			log.Errorf("drawCard err: 当前不允许摸牌，pending=%+v", t.pending)
+			return
+		}
+		t.onDrawCard(p)
+
+	case v1.ACTION_SKIP_TURN: // 8牌
+		if t.pending == nil || t.pending.Effect != v1.CARD_EFFECT_SUSPEND {
+			log.Errorf("skipCard err: 当前不允许跳过，pending=%+v", t.pending)
+			return
+		}
+		t.onSkipTurn(p)
+
+	case v1.ACTION_DECLARE_SUIT:
+		if t.pending == nil || t.pending.Effect != v1.CARD_EFFECT_WHOT || t.currCard != WhotCard {
+			log.Errorf("declareCard err: 当前不允许声明花色, pending=%+v currCard=%d", t.pending, t.currCard)
+			return
+		}
+		if suit := in.DeclareSuit; suit < v1.SUIT_CIRCLE || suit > v1.SUIT_START {
+			log.Errorf("declareCard err: 非法花色: %v", in.DeclareSuit)
+			return
+		}
+		t.onDeclareSuit(p, in.DeclareSuit)
+
+	default:
+		log.Warnf("未知操作类型: %v", in.Action)
+		return
 	}
+
 	return true
 }
 
-// see
-func (t *Table) canSeeCard(p *player.Player) ActionRet {
-	if p == nil || p.Seen() {
-		return ActionRet{Code: ErrorAlreadySeen}
-	}
-	state := t.stage.GetState()
-	if state != StSendCard && state != StAction && state != StSideShow {
-		return ActionRet{Code: ErrInvalidStage}
-	}
-	return ActionRet{}
-}
+// 处理出牌逻辑
+func (t *Table) onPlayCard(p *player.Player, card int32) {
+	// 从手牌中移除
+	p.RemoveCard(card)
 
-func (t *Table) handleSee(p *player.Player, timeout bool) {
-	if ret := t.canSeeCard(p); ret.Code != ErrOK {
-		t.sendActionRsp(p, &v1.ActionRsp{Code: ret.Code, Action: v1.ACTION_SEE})
+	// 更新当前牌堆顶
+	t.currCard = card
+	t.declareSuit = v1.SUIT_INVALID
+
+	// 清除 pending 或设置新效果
+	t.updatePending(p, card)
+
+	// 广播出牌
+	t.broadcastPlayerAction(p, v1.ACTION_PLAY_CARD, []int32{card}, 0)
+
+	// 判断是否出完了
+	if len(p.GetCards()) == 0 {
+		t.updateStage(StWaitEnd) // 等待结束
 		return
 	}
 
-	p.SetSeen()
-	p.SetLastOp(v1.ACTION_SEE)
-	p.IncrTimeoutCnt(timeout)
-	t.broadcastActionRsp(p, v1.ACTION_SEE, 0, nil, false)
-	t.mLog.SeeCard(p)
-
-	if p.GetChairID() == t.active {
-		// 刷新定时器，通知活动玩家
-		t.active = p.GetChairID()
-		t.updateStage(StAction)
-		t.broadcastActivePlayerPush()
-	} else {
-		// 判断当前活动玩家是否可以发起比牌
-		t.sendActiveButtonInfoNtf()
+	// 14牌：所有其他玩家各抽一张 MARKET, 发牌不够了游戏结束
+	if t.pending != nil && t.pending.Effect == v1.CARD_EFFECT_MARKET && Number(card) == 14 {
+		if end := t.broadDrawCardByMarket(); end {
+			t.updateStage(StWaitEnd)
+			return
+		}
 	}
+
+	log.Debugf("==> playCard. p:%s currCard:%v pending:%v", p.Desc(), t.currCard, t.pending)
+
+	// 通知下个玩家操作
+	t.active = t.getNextActiveChair()
+	if t.pending != nil {
+		t.active = t.pending.Target // 绑定玩家为活动玩家
+	}
+	t.updateStage(StPlaying)
+	t.broadcastActivePlayerPush()
 }
 
-// 弃牌 允许非当前玩家操作
-func (t *Table) canPack(p *player.Player) ActionRet {
-	state := t.stage.GetState()
-	// 比牌阶段不可丢牌
-	if p == nil || !p.IsGaming() || state == StSideShow {
-		return ActionRet{Code: ErrInvalidStage}
-	}
-	return ActionRet{}
-}
-
-func (t *Table) handlePack(p *player.Player, in *v1.ActionReq, timeout bool) {
-	if ret := t.canPack(p); ret.Code != ErrOK {
-		t.sendActionRsp(p, &v1.ActionRsp{Code: ret.Code, Action: v1.ACTION_PACK})
+func (t *Table) updatePending(p *player.Player, card int32) {
+	t.pending = nil
+	if !IsSpecialCard(card) {
 		return
 	}
 
-	p.SetFold() // 弃牌标记
-	t.addBetInfo(p, in.Action, timeout, 0)
-	t.broadcastActionRsp(p, in.Action, 0, nil, false)
-	t.mLog.PackCard(p, timeout)
+	nextChair := int32(-1)
+	if next := t.NextPlayer(p.GetChairID()); next != nil {
+		nextChair = next.GetChairID()
+	}
 
-	if ps := t.GetGamers(); len(ps) <= 1 {
+	target := p.GetChairID()
+	quantity := int32(1)
+	effect := v1.CARD_EFFECT_NORMAL
+
+	switch Number(card) {
+	case 1:
+		effect = v1.CARD_EFFECT_HOLD_ON
+	case 2:
+		effect = v1.CARD_EFFECT_PICK_TWO
+		target = nextChair
+		quantity = 2
+	case 8:
+		effect = v1.CARD_EFFECT_SUSPEND
+		target = nextChair
+	case 14:
+		effect = v1.CARD_EFFECT_MARKET
+	case 20:
+		effect = v1.CARD_EFFECT_WHOT
+	}
+
+	t.pending = &v1.Pending{
+		Initiator: p.GetChairID(),
+		Target:    target,
+		Effect:    effect,
+		Quantity:  quantity,
+	}
+}
+
+func (t *Table) broadDrawCardByMarket() (end bool) {
+	for _, v := range t.seats {
+		if v == nil || !v.IsGaming() {
+			continue
+		}
+		drawn := t.cards.DispatchCards(1)
+		if len(drawn) == 0 || t.cards.IsEmpty() {
+			return true
+		}
+		t.sendDrawCardPush(v, drawn)
+	}
+	return false
+}
+
+// 处理摸牌逻辑
+func (t *Table) onDrawCard(p *player.Player) {
+	count := int32(1)
+	if t.pending != nil && t.pending.Quantity > 0 {
+		count = t.pending.Quantity
+	}
+
+	drawn := t.cards.DispatchCards(int(count))
+	if len(drawn) == 0 || t.cards.IsEmpty() {
+		log.Debugf("无法摸牌（可能牌堆不足），结束游戏")
 		t.updateStage(StWaitEnd)
 		return
 	}
 
-	next := t.getNextActiveChair()
-	if p.GetChairID() == t.first {
-		t.first = next
+	// 处理pending状态
+	if t.pending != nil && t.pending.Target == p.GetChairID() {
+		log.Debugf("drawCard. p=%v 响应了 pending，清除: %+v", p.Desc(), t.pending)
+		t.pending = nil
 	}
-	if p.GetChairID() == t.active {
-		// 通知下个玩家操作
-		t.active = next
-		t.checkRound(t.active)
-		t.updateStage(StAction)
-		t.broadcastActivePlayerPush()
-	}
-}
 
-// 跟注（Call） 加注（Raise）
-func (t *Table) canCallCard(p *player.Player, isRaise bool) (callRes ActionRet) {
-	state := t.stage.GetState()
-	if p == nil || p.GetChairID() != t.active || state != StAction {
-		return ActionRet{Code: ErrInvalidStage}
-	}
-	callMoney := t.calcBetMoney(p)
-	if isRaise {
-		callMoney *= 2
-	}
-	if !t.hasEnoughMoney(p, callMoney) {
-		return ActionRet{Code: ErrNotEnoughMoney}
-	}
-	return ActionRet{Code: ErrOK, Money: callMoney}
-}
-
-func (t *Table) handleCall(p *player.Player, in *v1.ActionReq, timeout bool) {
-	callRaise := in.Action == v1.ACTION_RAISE
-	ret := t.canCallCard(p, callRaise)
-	if ret.Code != ErrOK {
-		if ret.Code == ErrNotEnoughMoney {
-			t.OnActionReq(p, &v1.ActionReq{Action: v1.ACTION_PACK}, false) // 直接弃牌处理
-		}
-		return
-	}
-	needMoney := ret.Money
-
-	t.addBetInfo(p, in.Action, timeout, needMoney)
-	t.broadcastActionRsp(p, in.Action, needMoney, nil, false)
-	t.mLog.CallCard(p, needMoney, callRaise, timeout)
-
-	// 判断是否需要处理所有比牌
-	if t.totalBet >= t.repo.GetRoomConfig().Game.PotLimit {
-		t.dealCompare(t.GetGamers(), CompareAllShow) // 处理所有比牌
-		return
-	}
+	p.AddCards(drawn)
+	t.broadcastPlayerAction(p, v1.ACTION_DRAW_CARD, drawn, 0)
 
 	// 通知下个玩家操作
 	t.active = t.getNextActiveChair()
-	t.checkRound(t.active)
-	t.updateStage(StAction)
+	t.updateStage(StPlaying)
 	t.broadcastActivePlayerPush()
 }
 
-// Show
-// 当只剩 2 名玩家时，任意一方可请求 Show
-// 明牌比较三张牌，胜者赢取全部筹码
-func (t *Table) canShowCard(p *player.Player) ActionRet {
-	state := t.stage.GetState()
-	if p == nil || p.GetChairID() != t.active || state != StAction || len(t.GetGamers()) != 2 {
-		return ActionRet{Code: ErrInvalidStage}
-	}
-	next := t.NextPlayer(p.GetChairID())
-	if next == nil {
-		return ActionRet{Code: ErrTargetInvalid}
-	}
-	money := t.calcBetMoney(p)
-	if !t.hasEnoughMoney(p, money) {
-		return ActionRet{Code: ErrNotEnoughMoney}
-	}
-	return ActionRet{Code: ErrOK, Money: money, Target: next}
-}
+// 处理跳过逻辑
+func (t *Table) onSkipTurn(p *player.Player) {
+	log.Debugf("skipTurn. p=%v 清除:%+v", p.Desc(), t.pending)
+	t.pending = nil
 
-func (t *Table) handleShow(p *player.Player, in *v1.ActionReq, timeout bool) {
-	ret := t.canShowCard(p)
-	if ret.Code != ErrOK {
-		if ret.Code == ErrNotEnoughMoney {
-			t.OnActionReq(p, &v1.ActionReq{Action: v1.ACTION_PACK}, false) // 直接弃牌处理
-		}
-		return
-	}
-	needMoney := ret.Money
+	t.broadcastPlayerAction(p, v1.ACTION_SKIP_TURN, nil, 0)
 
-	t.addBetInfo(p, in.Action, timeout, needMoney)
-	t.broadcastActionRsp(p, in.Action, needMoney, ret.Target, false)
-	t.mLog.ShowCard(p, ret.Target, needMoney, timeout)
-	t.dealCompare(t.GetGamers(), CompareShow) // 处理比牌 2个玩家
-}
-
-// Side Show
-// 剩余玩家数量 > 2
-// 仅限明注玩家对上一位明注玩家请求比牌
-// 若对方同意，则比大小，小的一方自动弃牌
-func (t *Table) canSideShowCard(p *player.Player) ActionRet {
-	state := t.stage.GetState()
-	if p == nil || p.GetChairID() != t.active || state != StAction || len(t.GetGamers()) <= 2 {
-		return ActionRet{Code: ErrInvalidStage}
-	}
-	last := t.LastPlayer(p.GetChairID())
-	if last == nil || last == p {
-		return ActionRet{Code: ErrTargetInvalid}
-	}
-	if !last.Seen() || !p.Seen() {
-		return ActionRet{Code: ErrSideNotSeen}
-	}
-	needMoney := t.calcBetMoney(p)
-	if !t.hasEnoughMoney(p, needMoney) {
-		return ActionRet{Code: ErrNotEnoughMoney}
-	}
-	return ActionRet{Code: ErrOK, Money: needMoney, Target: last}
-}
-
-func (t *Table) handleSideShow(p *player.Player, in *v1.ActionReq, timeout bool) {
-	ret := t.canSideShowCard(p)
-	if ret.Code != ErrOK {
-		if ret.Code == ErrNotEnoughMoney {
-			t.OnActionReq(p, &v1.ActionReq{Action: v1.ACTION_PACK}, false) // 直接弃牌处理
-		}
-		return
-	}
-	needMoney := ret.Money
-	last := ret.Target
-
-	t.addBetInfo(p, in.Action, timeout, needMoney)
-	t.broadcastActionRsp(p, in.Action, needMoney, last, false)
-	t.mLog.SidedShow(p, ret.Target, needMoney, timeout)
-	// log.Debugf("sideshow lunch. from:%v to:%v timeout:%v", p.Desc(), last.Desc(), timeout)
-
-	// 判断是否需要处理所有比牌
-	if t.totalBet >= t.repo.GetRoomConfig().Game.PotLimit {
-		t.dealCompare(t.GetGamers(), CompareAllShow) // 处理所有比牌
-		return
-	}
-
-	// 设置比牌的玩家椅子
-	p.SetCompareSeats([]int32{last.GetChairID()})
-
-	// 等待上家提前比牌回应
-	t.active = last.GetChairID()
-	t.updateStage(StSideShow)
+	// 通知下个玩家操作
+	t.active = t.getNextActiveChair()
+	t.updateStage(StPlaying)
 	t.broadcastActivePlayerPush()
 }
 
-// Side Show Reply
-// 能否回应提前比牌
-func (t *Table) canSideShowReply(p *player.Player) ActionRet {
-	state := t.stage.GetState()
-	if p == nil || p.GetChairID() != t.active || state != StSideShow || len(t.GetGamers()) <= 2 {
-		return ActionRet{Code: ErrInvalidStage}
-	}
-	next := t.NextPlayer(p.GetChairID())
-	if next == nil || !next.IsGaming() || !next.Seen() {
-		return ActionRet{Code: ErrTargetInvalid}
-	}
-	if !ext.SliceContains(next.GetCompareSeats(), p.GetChairID()) {
-		return ActionRet{Code: ErrTargetInvalid}
-	}
-	return ActionRet{Code: ErrOK, Target: next}
+// 处理声明花色逻辑
+func (t *Table) onDeclareSuit(p *player.Player, suit v1.SUIT) {
+	log.Debugf("declareSuit. p=%v suit=%v pending=%+v", p.Desc(), suit, t.pending)
+	t.currCard = NewDeclareWhot(int32(suit), t.currCard) // 修改当前牌的花色
+	t.declareSuit = suit
+	t.pending = nil // 声明后 pending 清除
+	t.broadcastPlayerAction(p, v1.ACTION_DECLARE_SUIT, nil, suit)
+
+	// 通知当前玩家操作
+	t.active = p.GetChairID()
+	t.updateStage(StPlaying)
+	t.broadcastActivePlayerPush()
 }
 
-// 回应提前比牌
-func (t *Table) handleSideShowReply(p *player.Player, in *v1.ActionReq, timeout bool) {
-	ret := t.canSideShowReply(p)
-	if ret.Code != ErrOK {
-		return
-	}
-	next := ret.Target
-
-	p.SetLastOp(in.Action)
-	p.IncrTimeoutCnt(timeout)
-	t.broadcastActionRsp(p, in.Action, 0, next, in.SideReplyAllow)
-	t.mLog.SideShowReply(p, ret.Target, in.SideReplyAllow, timeout)
-	// log.Debugf("sideshow Reply. from:%v tar:%v timeout:%v", p.Desc(), next.Desc(), timeout)
-
-	// 拒绝比牌
-	if !in.SideReplyAllow {
-		// 通知发起比牌玩家的下家操作
-		t.active = t.NextPlayer(next.GetChairID()).GetChairID()
-		t.checkRound(t.active)
-		t.updateStage(StAction)
-		t.broadcastActivePlayerPush()
-		return
-	}
-
-	// 同意比牌
-	winner, losses := t.dealCompare([]*player.Player{p, next}, CompareSideShow)
-	loss := losses[0]
-	if loss.GetChairID() == t.first {
-		t.first = t.NextPlayer(loss.GetChairID()).GetChairID()
-	}
-
-	// 通知赢家操作
-	t.active = winner.GetChairID()
-	t.updateStage(StSideShowAni)
-}
-
-func (t *Table) addBetInfo(p *player.Player, action v1.ACTION, timeout bool, bet float64) {
-	switch action {
-	// 统计下注额,超时次数
-	case v1.ACTION_PACK, v1.ACTION_CALL, v1.ACTION_RAISE, v1.ACTION_SHOW, v1.ACTION_SIDE:
-		p.UseMoney(bet)
-		p.AddBet(bet)
-		p.IncrPlayCnt()
-		p.SetLastOp(action)
-		p.IncrTimeoutCnt(timeout)
-		t.totalBet += bet // 累加
-	}
-
-	// 更新桌面下注额
-	if action == v1.ACTION_RAISE {
-		t.curBet *= 2
-	}
-}
-
-func (t *Table) checkRound(active int32) {
-	// 是否更新回合
-	if active == t.first {
-		t.curRound++
-	}
-
-	// 自动操作看
-	if t.curRound >= t.repo.GetRoomConfig().Game.AutoSeeRound {
-		for _, p := range t.seats {
-			if p == nil || !p.IsGaming() || p.Seen() {
-				continue
-			}
-			t.OnActionReq(p, &v1.ActionReq{Action: v1.ACTION_SEE}, false)
+// 判断给出的牌是否可以合法出牌
+func (t *Table) canOutCard(curr int32, hand []int32, card int32) bool {
+	canOuts := calcCanOut(curr, hand)
+	for _, c := range canOuts {
+		if c == card {
+			return true
 		}
 	}
+	return false
 }
 
-// 处理玩家比牌
-func (t *Table) dealCompare(compares []*player.Player, kind CompareType) (winner *player.Player, loss []*player.Player) {
-	if len(compares) == 0 {
-		return nil, nil
-	}
-	winner = compares[0]
-	for i, v := range compares {
-		if i == 0 {
-			winner = v
-			continue
-		}
-		w, l := compareCard(winner, v)
-		winner = w
-		loss = append(loss, l)
-		l.SetLost() // 比牌输家标记
+// 判断是否可以摸牌（根据当前pending效果）
+func (t *Table) canDrawCard(p *player.Player) bool {
+	pending := t.pending
+
+	if pending != nil && pending.Target != p.GetChairID() {
+		log.Errorf("drawCard err: 非目标玩家尝试摸牌: %v", p.Desc())
+		return false
 	}
 
-	t.mLog.compareCard(kind, winner, loss)
-	log.Debugf("[玩家比牌] %s", logCompare(kind, winner, loss))
-	if len(t.GetGamers()) <= 1 {
-		t.updateStage(StWaitEnd) // 等待结束
+	if pending == nil {
+		return true
 	}
-	return
-}
-
-func compareCard(p1, p2 *player.Player) (winner, loss *player.Player) {
-	if calc.Compare(p1.GetHand(), p2.GetHand()) {
-		return p1, p2
+	switch pending.Effect {
+	case v1.CARD_EFFECT_NORMAL, v1.CARD_EFFECT_HOLD_ON, v1.CARD_EFFECT_PICK_TWO:
+		return true
+	default:
+		return false
 	}
-	return p2, p1
-}
-
-// calcBetMoney 计算当前玩家根据是否看牌应支付的金额
-func (t *Table) calcBetMoney(p *player.Player) float64 {
-	if p.Seen() {
-		return t.curBet * 2
-	}
-	return t.curBet
-}
-
-// hasEnoughMoney 检查玩家是否有足够的钱下注
-func (t *Table) hasEnoughMoney(p *player.Player, amount float64) bool {
-	return p.GetAllMoney() >= amount
 }
