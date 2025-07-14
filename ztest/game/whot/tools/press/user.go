80fd4f1c683612d03a016b776497d15d4d0b3e0a
zhangyh <1518684794@qq.com>
2025-07-14 18:27:12 +0800

    whot client

--- ztest/game/whot/tools/press/user.go
+++ ztest/game/whot/tools/press/user.go
@@ -10,7 +10,6 @@ import (
 	"github.com/yola1107/kratos/v2/log"
 	"github.com/yola1107/kratos/v2/transport/websocket"
 	v1 "github.com/yola1107/kratos/v2/ztest/game/whot/api/helloworld/v1"
-	"github.com/yola1107/kratos/v2/ztest/game/whot/internal/biz/table"
 	gproto "google.golang.org/protobuf/proto"
 )
 
@@ -92,32 +91,29 @@ func (u *User) connect() (*websocket.Client, error) {
 		int32(v1.GameCommand_OnChatRsp):            u.OnEmptyPush,  // GameCommand = 1012
 		int32(v1.GameCommand_OnHostingRsp):         u.OnEmptyPush,  // GameCommand = 1014
 		int32(v1.GameCommand_OnForwardRsp):         u.OnEmptyPush,  // GameCommand = 1016
-		int32(v1.GameCommand_OnActionRsp):          u.OnActionRsp,  // GameCommand = 1102
-		int32(v1.GameCommand_OnAutoCallRsp):        u.OnEmptyPush,  // GameCommand = 1104
+		int32(v1.GameCommand_OnPlayerActionRsp):    u.OnActionRsp,  // GameCommand = 1102
 		int32(v1.GameCommand_OnUserInfoPush):       u.OnEmptyPush,  // GameCommand = 2001
 		int32(v1.GameCommand_OnEmojiConfigPush):    u.OnEmptyPush,  // GameCommand = 2002
 		int32(v1.GameCommand_OnPlayerQuitPush):     u.OnEmptyPush,  // GameCommand = 2003
 		int32(v1.GameCommand_OnUserOfflinePush):    u.OnEmptyPush,  // GameCommand = 2004
-		int32(v1.GameCommand_OnSetBankerPush):      u.OnEmptyPush,  // GameCommand = 2100
+		int32(v1.GameCommand_OnMatchResultPush):    u.OnEmptyPush,  // GameCommand = 2100
 		int32(v1.GameCommand_OnSendCardPush):       u.OnEmptyPush,  // GameCommand = 2101
 		int32(v1.GameCommand_OnActivePush):         u.OnActivePush, // GameCommand = 2102 //活动玩家通知
-		int32(v1.GameCommand_OnRoundSeePush):       u.OnEmptyPush,  // GameCommand = 2103
-		int32(v1.GameCommand_OnAfterSeeButtonPush): u.OnEmptyPush,  // GameCommand = 2104
-		int32(v1.GameCommand_OnShowCardPush):       u.OnEmptyPush,  // GameCommand = 2105
+		int32(v1.GameCommand_OnMarketDrawCardPush): u.OnEmptyPush,  // GameCommand = 2103
+		int32(v1.GameCommand_OnLastCardPush):       u.OnEmptyPush,  // GameCommand = 2104
 		int32(v1.GameCommand_OnResultPush):         u.OnResultPush, // GameCommand = 2200 //结算通知
 	}
 	rspHandler := map[int32]websocket.ResponseHandler{
-		int32(v1.GameCommand_SayHelloReq):      u.OnEmptyRequest, // 空
-		int32(v1.GameCommand_OnLoginReq):       u.OnEmptyRequest, // GameCommand = 1001 //登录
-		int32(v1.GameCommand_OnLogoutReq):      u.OnEmptyRequest, // GameCommand = 1003 //登出
-		int32(v1.GameCommand_OnReadyReq):       u.OnEmptyRequest, // GameCommand = 1005 //准备
-		int32(v1.GameCommand_OnSwitchTableReq): u.OnEmptyRequest, // GameCommand = 1007 //换桌
-		int32(v1.GameCommand_OnSceneReq):       u.OnEmptyRequest, // GameCommand = 1009 //场景信息
-		int32(v1.GameCommand_OnChatReq):        u.OnEmptyRequest, // GameCommand = 1011 //聊天
-		int32(v1.GameCommand_OnHostingReq):     u.OnEmptyRequest, // GameCommand = 1013 //托管
-		int32(v1.GameCommand_OnForwardReq):     u.OnEmptyRequest, // GameCommand = 1015 //转发
-		int32(v1.GameCommand_OnActionReq):      u.OnEmptyRequest, // GameCommand = 1101 //玩家动作
-		int32(v1.GameCommand_OnAutoCallReq):    u.OnEmptyRequest, // GameCommand = 1103 //自动跟注
+		int32(v1.GameCommand_SayHelloReq):       u.OnEmptyRequest, // 空
+		int32(v1.GameCommand_OnLoginReq):        u.OnEmptyRequest, // GameCommand = 1001 //登录
+		int32(v1.GameCommand_OnLogoutReq):       u.OnEmptyRequest, // GameCommand = 1003 //登出
+		int32(v1.GameCommand_OnReadyReq):        u.OnEmptyRequest, // GameCommand = 1005 //准备
+		int32(v1.GameCommand_OnSwitchTableReq):  u.OnEmptyRequest, // GameCommand = 1007 //换桌
+		int32(v1.GameCommand_OnSceneReq):        u.OnEmptyRequest, // GameCommand = 1009 //场景信息
+		int32(v1.GameCommand_OnChatReq):         u.OnEmptyRequest, // GameCommand = 1011 //聊天
+		int32(v1.GameCommand_OnHostingReq):      u.OnEmptyRequest, // GameCommand = 1013 //托管
+		int32(v1.GameCommand_OnForwardReq):      u.OnEmptyRequest, // GameCommand = 1015 //转发
+		int32(v1.GameCommand_OnPlayerActionReq): u.OnEmptyRequest, // GameCommand = 1101 //玩家动作
 	}
 	return websocket.NewClient(
 		u.repo.GetContext(),
@@ -181,32 +177,25 @@ func (u *User) OnActivePush(data []byte) {
 	if u.chair.Load() != rsp.Active {
 		return
 	}
-	op := table.RandOpWithWeight(rsp.CanOp)
-	req := &v1.ActionReq{
-		UserID:         u.id,
-		Action:         op,
-		SideReplyAllow: ext.IsHitFloat(0.3),
-	}
-	dur := time.Duration(ext.RandInt(0, 12000)) * time.Millisecond
-	u.repo.GetTimer().Once(dur, func() {
-		u.Request(v1.GameCommand_OnActionReq, req)
-	})
+	// op := table.RandOpWithWeight(rsp.CanOp)
+	// req := &v1.ActionReq{
+	// 	UserID:         u.id,
+	// 	Action:         op,
+	// 	SideReplyAllow: ext.IsHitFloat(0.3),
+	// }
+	// dur := time.Duration(ext.RandInt(0, 12000)) * time.Millisecond
+	// u.repo.GetTimer().Once(dur, func() {
+	// 	u.Request(v1.GameCommand_OnActionReq, req)
+	// })
 }
 
 func (u *User) OnActionRsp(data []byte) {
-	rsp := &v1.ActionRsp{}
+	rsp := &v1.PlayerActionRsp{}
 	if err := gproto.Unmarshal(data, rsp); err != nil {
 		log.Errorf("%v", err)
 		return
 	}
 	if rsp.Code != 0 {
-		return
-	}
-	if rsp.UserID != u.id {
-		return
-	}
-	if rsp.Action == v1.ACTION_PACK && ext.IsHitFloat(u.repo.GetConfig().LogoutRate) {
-		u.sendLogoutReq()
 		return
 	}
 }
@@ -217,12 +206,15 @@ func (u *User) OnResultPush(data []byte) {
 		log.Errorf("%v", err)
 		return
 	}
-	if rsp.UserID != u.id {
-		return
-	}
-	if ext.IsHitFloat(u.repo.GetConfig().LogoutRate) {
-		u.sendLogoutReq()
-		return
+
+	for _, v := range rsp.Results {
+		if v.UserID != u.id {
+			continue
+		}
+		if ext.IsHitFloat(u.repo.GetConfig().LogoutRate) {
+			u.sendLogoutReq()
+			return
+		}
 	}
 	u.chair.Store(-1)
 }
